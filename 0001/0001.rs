1be3a585760963fce6ba6072036ac843d90d18b2
positoro <positoro@gmail.com>
2023-04-30 19:57:41 +0900

    0001

--- 0001/0001.rs
+++ 0001/0001.rs
@@ -1,3 +1,11 @@
 fn main() {
-println!("sum: {}", sum);
+    let mut sum: u32 = 0;
+
+    for i in 1..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
+        }
+    }
+
+    println!("sum: {}", sum);
 }
