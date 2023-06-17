72d70392198a4172603cc016ba9b0014bd0a52fc
positoro <positoro@gmail.com>
2023-06-18 04:34:20 +0900

    0003

--- 0003/0003.rs
+++ 0003/0003.rs
@@ -1,38 +1,36 @@
 fn main() {
     let prime_factor_range: usize = 10000;
     let mut the_number: u64 = 600851475143;
-    let prime_factor_vec: Vec<u64> = sieve_of_eratosthenes(prime_factor_range);
-    let mut prime_factor_vec2: Vec<u64> = Vec::new();
+    let the_prime_factor_vec: Vec<u64> = sieve_of_eratosthenes(prime_factor_range);
+    let mut prime_factor_in_the_number: Vec<u64> = Vec::new();
 
-    for i in 0..prime_factor_vec.len() {
-        while the_number % prime_factor_vec[i] == 0 {
-            the_number = the_number / prime_factor_vec[i];
-            prime_factor_vec2.push(prime_factor_vec[i]);
+    for i in 0..the_prime_factor_vec.len() {
+        while the_number % the_prime_factor_vec[i] == 0 {
+            the_number = the_number / the_prime_factor_vec[i];
+            prime_factor_in_the_number.push(the_prime_factor_vec[i]);
         }
         if the_number == 1 {
             break;
         }
     }
-    println!("{:?}", prime_factor_vec2);
+    println!("{:?}", prime_factor_in_the_number);
 }
 
 fn sieve_of_eratosthenes(n: usize) -> Vec<u64> {
     let mut return_vec: Vec<u64> = Vec::new();
-    let mut vec: Vec<u64> = vec![1; n];
+    let mut vec: Vec<bool> = vec![true; n];
 
-    vec[0] = 0;
-    vec[1] = 0;
+    vec[0] = false;
+    vec[1] = false;
 
-    for p in 2..n {
-        let mut i: usize = 2;
-        while i * p < n {
-            vec[i * p] = 0;
-            i = i + 1;
+    for i in 2..(n as f64).sqrt() as usize {
+        for j in 2..(n as f64).sqrt() as usize {
+            vec[i * j] = false;
         }
     }
 
     for (i, value) in vec.iter().enumerate() {
-        if *value == 1 {
+        if *value == true {
             return_vec.push(i as u64);
         }
     }
