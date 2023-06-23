7460489907b648187a9cd62ae814864787362c99
positoro <positoro@gmail.com>
2023-06-23 19:39:27 +0900

    continued

--- 0002/0002.rs
+++ 0002/0002.rs
@@ -1,16 +1,30 @@
 fn main() {
-    let mut sum: u32 = 2;
-    let mut a: u32 = 1;
-    let mut b: u32 = 2;
-    let mut c: u32 = 0;
-    while c <= 4000000 {
-        c = a + b;
-        if c % 2 == 0 {
-            sum += c;
-        }
-        a = b;
-        b = c;
+    let mut sum: u32 = 0;
+    let mut the_fibonacci_sequence_vec: Vec<u32> = Vec::new();
+
+    const first_sequence = 1;
+    const second_sequence = 2;
+
+    if add_last_the_fibonacci_sequence_vec_and_is_last_even(first_sequence, &the_fibonacci_sequence_vec) {
+        sum += first_sequence;
+    }
+
+    if add_last_the_fibonacci_sequence_vec_and_is_last_even(second_sequence, &the_fibonacci_sequence_vec) {
+        sum += second_sequence;
+    }
+    let mut last_sequence= first_sequence + second_sequence;
+
+    while last_sequence <= 4000000 {
+    if add_last_the_fibonacci_sequence_vec_and_is_last_even(last_sequence, &the_fibonacci_sequence_vec) {
+        sum += last_sequence;
+    }
     }
 
     println!("sum: {}", sum);
+}
+
+fn add_last_the_fibonacci_sequence_vec_and_is_last_even(
+    n: u32,
+    the_fibonacci_sequence_vec: &Vec<u32>,
+) -> bool {
 }
