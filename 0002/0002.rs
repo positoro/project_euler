2b21e0b663cb9fd952ce2323889412f53b99c26f
positoro <positoro@gmail.com>
2023-06-25 19:20:31 +0900

    0002

--- 0002/0002.rs
+++ 0002/0002.rs
@@ -1,30 +1,35 @@
 fn main() {
+    let mut array_for_the_fibonacci_sequence: [u32; 3] = [0; 3];
+
+    const FIRST_SEQUENCE: u32 = 1;
+    const SECOND_SEQUENCE: u32 = 2;
+
     let mut sum: u32 = 0;
-    let mut the_fibonacci_sequence_vec: Vec<u32> = Vec::new();
+    let mut last_sequence: u32 = 0;
 
-    const first_sequence = 1;
-    const second_sequence = 2;
-
-    if add_last_the_fibonacci_sequence_vec_and_is_last_even(first_sequence, &the_fibonacci_sequence_vec) {
-        sum += first_sequence;
-    }
-
-    if add_last_the_fibonacci_sequence_vec_and_is_last_even(second_sequence, &the_fibonacci_sequence_vec) {
-        sum += second_sequence;
-    }
-    let mut last_sequence= first_sequence + second_sequence;
+    array_for_the_fibonacci_sequence[0] = FIRST_SEQUENCE;
+    check_even_and_add(FIRST_SEQUENCE, &mut sum);
+    array_for_the_fibonacci_sequence[1] = SECOND_SEQUENCE;
+    check_even_and_add(SECOND_SEQUENCE, &mut sum);
 
     while last_sequence <= 4000000 {
-    if add_last_the_fibonacci_sequence_vec_and_is_last_even(last_sequence, &the_fibonacci_sequence_vec) {
-        sum += last_sequence;
-    }
+        last_sequence = proceed_fibonacci_sequence(&mut array_for_the_fibonacci_sequence);
+        check_even_and_add(last_sequence, &mut sum);
     }
 
     println!("sum: {}", sum);
 }
 
-fn add_last_the_fibonacci_sequence_vec_and_is_last_even(
-    n: u32,
-    the_fibonacci_sequence_vec: &Vec<u32>,
-) -> bool {
+fn check_even_and_add(n: u32, sum: &mut u32) {
+    if n % 2 == 0 {
+        *sum += n;
+    }
+}
+
+fn proceed_fibonacci_sequence(array: &mut [u32; 3]) -> u32 {
+    array[2] = array[0] + array[1];
+    array[0] = array[1];
+    array[1] = array[2];
+
+    return array[2];
 }
