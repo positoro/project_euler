f82ed7c8702c748d9b1c4f3db22ff72d27cebd47
positoro <positoro@gmail.com>
2023-08-08 18:16:54 +0900

    AC

--- 0007/0007.rs
+++ 0007/0007.rs
@@ -1,22 +1,17 @@
 fn main() {
-    let the_prime_number_vec: Vec<u64> = create_prime_number_vec(200000);
+    let the_prime_number_vec: Vec<u64> = create_prime_number_vec(104744);
 
-    for i in 0..the_prime_number_vec.len() as usize {
-        println!("{}, {}", the_prime_number_vec[i], i);
-    }
+    println!("10001th prime number is {}", the_prime_number_vec[10000]);
 }
 
 fn create_prime_number_vec(n: usize) -> Vec<u64> {
     let mut return_vec: Vec<u64> = Vec::new();
     let mut vec: Vec<bool> = vec![true; n];
-
     vec[0] = false;
     vec[1] = false;
-    vec[4] = false;
-    vec[6] = false;
-    vec[8] = false;
+
     for i in 2..n {
-        for j in 2..(i as f64).sqrt() as usize {
+        for j in 2..((i as f64).sqrt() + 1.0) as usize {
             if i % j == 0 {
                 vec[i] = false;
             };
