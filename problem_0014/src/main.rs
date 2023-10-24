c8027ad3a176e6ce9949b202fb5dafc9dd71b5f4
positoro <positoro@gmail.com>
2023-10-24 14:58:38 +0900

    0014 AC

--- problem_0014/src/main.rs
+++ problem_0014/src/main.rs
@@ -1,29 +1,29 @@
 fn main() {
-    const UPPER: u32 = 1000000;
-    let mut max_counts: u128 = 0;
+    const UPPER: u128 = 1000000;
+    let mut max_counts: u32 = 0;
+    let mut max_index: u128 = 0;
 
-    for i in 0..UPPER {
-        let counts: u128 = get_chain_counts(i);
+    for i in 1..UPPER {
+        let counts: u32 = get_chain_terms(i);
         if max_counts < counts {
             max_counts = counts;
+            max_index = i;
         }
     }
-    println!("{:?}", max_counts);
+    println!("{:?} contains {:?} terms.", max_index, max_counts);
 }
 
-fn get_chain_counts(mut s: u32) -> u128 {
-    let mut return_counts: u128 = 0;
-    s = 159487;
+fn get_chain_terms(mut s: u128) -> u32 {
+    let mut return_terms: u32 = 1;
 
     while s > 1 {
-        return_counts = return_counts + 1;
+        return_terms = return_terms + 1;
         if s % 2 == 0 {
             s = s / 2;
         } else {
             s = s * 3 + 1;
-            println!("{:?}", s);
         }
     }
 
-    return return_counts;
+    return return_terms;
 }
